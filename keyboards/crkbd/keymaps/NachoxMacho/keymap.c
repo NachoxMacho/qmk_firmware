ccedda622297ea57f64183f5aef65a2c82b52b75
Rob Owens <rtowens17@gmail.com>
2022-01-29 11:16:27 -0600

    switch alt and shift keys

--- keyboards/crkbd/keymaps/NachoxMacho/keymap.c
+++ keyboards/crkbd/keymaps/NachoxMacho/keymap.c
@@ -70,9 +70,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_GAM2] = LAYOUT_split_3x6_3(
         KC_ESC,     KC_Q,    KC_W,    KC_E, KC_R, KC_T,      KC_Y,     KC_U,   KC_I,     KC_O,   KC_P,    KC_EQL,
-        KC_LALT,    KC_A,    KC_S,    KC_D, KC_F, KC_G,      KC_H,     KC_J,   KC_K,     KC_L,   KC_SCLN, KC_QUOT,
+        KC_LSHIFT,    KC_A,    KC_S,    KC_D, KC_F, KC_G,      KC_H,     KC_J,   KC_K,     KC_L,   KC_SCLN, KC_QUOT,
         KC_LCTRL,   KC_Z,    KC_X,    KC_C, KC_V, KC_B,      KC_N,     KC_M,   KC_COMM,  KC_DOT, KC_SLSH, KC_MINS,
-        KC_LSHIFT,  KC_SPC, KC_TAB,                         KC_ENT,   KC_BSPC, TO(_QWERTY)
+        KC_LALT,  KC_SPC, KC_TAB,                         KC_ENT,   KC_BSPC, TO(_QWERTY)
     ),
 
     [_FUN] = LAYOUT_split_3x6_3(
