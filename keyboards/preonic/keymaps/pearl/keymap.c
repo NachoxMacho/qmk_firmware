9065074cd7bf37d0a97dc58320634035c09ba599
Rob Owens <rtowens17@gmail.com>
2022-01-30 14:33:15 -0600

    update pearl's keymap

--- keyboards/preonic/keymaps/pearl/keymap.c
+++ keyboards/preonic/keymaps/pearl/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_DEL,      KC_Q,    KC_W,    KC_E,      KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_TAB,      KC_A,    KC_S,    KC_D,      KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   TG(_NUM),    KC_Z,    KC_X,    KC_C,      KC_V,    KC_B,    KC_N,    KC_M,    RCTL_T(KC_COMM), RSFT_T(KC_DOT),  RALT_T(KC_SLSH), KC_RGUI,
-  KC_LGUI, KC_LALT, KC_LSHIFT, KC_LCTRL,            KC_ENTER, KC_SPC,           KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT
+  KC_LSHIFT,   KC_LGUI, KC_LALT, KC_LCTRL,            KC_ENTER, KC_SPC,           KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT
 ),
 
 [_MOD] = LAYOUT_preonic_2x2u(
